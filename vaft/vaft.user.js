6fe3ebe4e1a1221a22fdcce23d08d1d2c98054c6
Diyor Sattarov <diyorsattarov@outlook.com>
2025-06-21 22:22:45 -0500

    Update vaft.user.js

--- vaft/vaft.user.js
+++ vaft/vaft.user.js
@@ -27,7 +27,7 @@
         scope.ClientSession = 'null';
         scope.PlayerType2 = 'embed'; //Source
         scope.PlayerType3 = 'site'; //Source
-        scope.PlayerType4 = 'autoplay'; //360p
+        scope.PlayerType4 = 'picture-by-picture'; //360p
         scope.CurrentChannelName = null;
         scope.UsherParams = null;
         scope.WasShowingAd = false;
